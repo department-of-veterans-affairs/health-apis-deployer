54a49a526a643417e3b865809f338be3bf182382
Bryan Schofield <bryan.schofield@libertyits.com>
2019-01-18 14:45:58 -0500

    Upgraderator test Fri Jan 18 14:45:58 EST 2019

--- upgraderator/upgraderator.sh
+++ upgraderator/upgraderator.sh
@@ -4,20 +4,32 @@ BASE=$(dirname $(readlink -f $0))
 
 BUILD_INFO=$BASE/build.conf
 CONF=$BASE/upgrade.conf
-
+ENV_CONF=
 [ -z "$ENVIRONMENT" ] && echo "Environment not specified" && exit 1
+case $ENVIRONMENT in
+  qa) ENV_CONF=qa.conf;;
+  *) echo "Unknown environment: $ENVIRONMENT" && exit 1
+esac
+
 [ -z "$BUILD_INFO" ] && echo "Build info file not found: $BUILD_INFO" && exit 1
 [ ! -f "$CONF" ] && echo "Configuration file not found: $CONF" && exit 1
 . $BUILD_INFO
 . $CONF
+. $ENV_CONF
 
 VERSION=${HEALTH_APIS_VERSION}-${BUILD_HASH}
 
 echo "Upgrading Health APIs in $ENVIRONMENT to $VERSION"
+cat $ENV_CONF | sort
 
 echo "Build info"
 cat $BUILD_INFO | sort
 
 echo "Configuration"
-echo $CONF | sort
+cat $CONF | sort
 
+echo ------------------------------------------------------------
+
+oc login $OPENSHIFT_URL --token $OPENSHIFT_API_TOKEN
+oc project $OPENSHIFT_PROJECT
+oc get pods
