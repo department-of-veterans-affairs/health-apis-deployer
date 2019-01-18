f20d1267a8fc84a470fc352a716d59850372b91d
Bryan Schofield <bryan.schofield@libertyits.com>
2019-01-18 13:49:00 -0500

    Upgraderator test Fri Jan 18 13:49:00 EST 2019

--- build.sh
+++ build.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 
-
-env | sort
-
 cd $(dirname $(readlink -f $0))
 . upgraderator/upgrade.conf
 
+env | sort
+
+
 HASH=$(git rev-parse --short HEAD)
 TAG=${HEALTH_APIS_VERSION}-${HASH}
-
+IMAGE="vasdvp/health-apis-upgraderator:$TAG"
 echo "Building upgraderator $TAG"
 
 cat <<EOF > upgraderator/build.conf
 BUILD_DATE="$(date)"
 BUILD_HASH=$HASH
+BUILD_ID=$BUILD_ID
+BUILD_BRANCH_NAME=$BRANCH_NAME
+BUILD_URL="$BUILD_URL"
 EOF
-docker build -t vasdvp/health-apis-upgraderator:$TAG .
+docker build -t $IMAGE .
 
+
+docker run \
+       --rm \
+       -e ENVIRONMENT=qa \
+       -e DOCKER_SOURCE_REGISTRY="$DOCKER_SOURCE_REGISTRY" \
+       -e DOCKER_USERNAME="$DOCKER_USERNAME" \
+       -e DOCKER_PASSWORD="$DOCKER_PASSWORD" \
+       -e OPENSHIFT_USERNAME="$OPENSHIFT_USERNAME" \
+       -e OPENSHIFT_PASSWORD="$OPENSHIFT_PASSWORD" \
+       $IMAGE
+      
--- upgraderator/upgraderator.sh
+++ upgraderator/upgraderator.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
 BASE=$(dirname $(readlink -f $0))
+
 BUILD_INFO=$BASE/build.conf
 CONF=$BASE/upgrade.conf
+
+[ -z "$ENVIRONMENT" ] && echo "Environment not specified" && exit 1
 [ -z "$BUILD_INFO" ] && echo "Build info file not found: $BUILD_INFO" && exit 1
 [ ! -f "$CONF" ] && echo "Configuration file not found: $CONF" && exit 1
 . $BUILD_INFO
 . $CONF
 
-cat <<EOF
-BUILD_DATE ............ $BUILD_DATE
-BUILD_HASH ............ $BUILD_HASH
-HEALTH_APIS_VERSION ... $HEALTH_APIS_VERSION
+VERSION=${HEALTH_APIS_VERSION}-${BUILD_HASH}
 
-EOF
+echo "Upgrading Health APIs in $ENVIRONMENT to $VERSION"
+
+echo "Build info"
+cat $BUILD_INFO | sort
+
+echo "Configuration"
+echo $CONF | sort
+
