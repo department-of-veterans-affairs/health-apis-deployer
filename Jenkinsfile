c5b120362b7f6d1cb1ba018f87b555a4706a6fe2
Bryan Schofield <bryan.schofield@libertyits.com>
2019-02-06 09:14:11 -0500

    Upgraderator test Wed Feb  6 09:14:11 EST 2019

--- Jenkinsfile
+++ Jenkinsfile
@@ -38,7 +38,32 @@ pipeline {
     upstream(upstreamProjects: 'department-of-veterans-affairs/health-apis/master', threshold: hudson.model.Result.SUCCESS)
   }
   stages {
+    /*
+     * Make sure we're getting into an infinite loop of build, commit, build because we committed.
+     */
+    stage('C-C-C-Combo Breaker!') {
+      steps {
+        script {
+          /*
+           * If you need the explanation for this, check out the function. Hard enough to explain once.
+           * tl;dr Github web hooks could cause go in an infinite loop.
+           */
+          if (checkBigBen()) {
+            mode = 'ignore'
+              /*
+               * OK, this is a janky hack! We don't want this job. We didn't want
+               * it to even start building, so we'll make it commit suicide! Build
+               * numbers will skip, but whatever, that's better than every other
+               * build being cruft.
+               */
+              currentBuild.result = 'NOT_BUILT'
+              currentBuild.rawBuild.delete()
+          }
+        }
+      } // steps
+    } // stage
     stage('Deploy') {
+      when { expression { return mode != 'ignore' } }
       steps {
         withCredentials([
           usernamePassword(
--- build.sh
+++ build.sh
@@ -1,18 +1,70 @@
 #!/usr/bin/env bash
 
 cd $(dirname $(readlink -f $0))/upgraderator
-. upgrade.conf
 
-env | sort
-echo ------------------------------------------------------------
+#
+# One of the following build cause environment variables will be set
+# based on the trigger that initiated the build.
+#
+# BUILD_TIMER_TRIGGER_CAUSE=Started by timer
+# BUILD_USER_ID_CAUSE=Started by user bryan.schofield
+# BUILD_UPSTREAM_CAUSE=Started by upstream project "department-of-veterans-affairs/health-apis/master" build number 6,993
+#
 
-HASH=${GIT_COMMIT:0:7}
-[ -z "$HASH" ] && HASH=DEV
-VERSION="${BUILD_ID:-NONE}-$(echo ${HEALTH_APIS_VERSION}|tr . -)-${HASH}"
-IMAGE="vasdvp/health-apis-upgraderator:$VERSION"
-echo "Building upgraderator $VERSION"
+updateToLatestHealthApis() {
+  echo ------------------------------------------------------------
+  echo "Determine latest version of Health APIs"
+  local latest=$(curl \
+    -s \
+    -u "$DOCKER_USERNAME:$DOCKER_PASSWORD" \
+    "https://registry.hub.docker.com/v1/repositories/vasdvp/health-apis-sentinel/tags" \
+    | jq -r .[].name \
+    | grep -E '([0-9]+.[0-9]+.[0-9]+)' \
+    | sort -rV \
+    | head -1)
+  [ -z "$latest" ] && echo "Could not determine latest version" && exit 1
+  . version.conf
+  [ "$latest" == "$HEALTH_APIS_VERSION" ] && echo "Already configured to use $latest" && return 0
+  echo "Updating to $latest"  
+  sed -i "s/HEALTH_APIS_VERSION=.*/HEALTH_APIS_VERSION=$latest/" version.conf
+  #
+  # This is annoying... Jenkins will checkout in a detached state. This means we
+  # won't be able to push changes... They suggest a doing the following shell
+  # step to check out the branch in an attached state. We'll also force our
+  # local copy of the branch to be reset to match the Origin.
+  #
+  # https://support.cloudbees.com/hc/en-us/articles/227266408-current-Git-branch-is-HEAD-detached-at
+  #
+  git checkout -B ${BRANCH_NAME} origin/${BRANCH_NAME}
+  #
+  # We'll need to push tags and pom changes back to Github. We'll do that with a user with
+  # elevated permissions. We'll compute a new "origin" that uses an access token. And for
+  # good measure, we'll print a little debugging information.
+  #
+  ORIGIN=$(git remote get-url --push origin | sed "s|https://|https://$GITHUB_USERNAME_PASSWORD@|")
+  git config --replace-all user.email 'vasdvp.jenkins@libertyits.com'
+  git config --replace-all user.name 'jenkins'
+  #
+  # Now we can add our changes
+  #
+  git add version.conf
+  git commit -m "Jenkins updated HEALTH_APIS_VERSION=$latest"
+  git push $ORIGIN $BRANCH_NAME
+}
 
-cat <<EOF > build.conf
+
+configureUpgraderator() {
+  echo ------------------------------------------------------------
+  . version.conf
+  env | sort
+
+  HASH=${GIT_COMMIT:0:7}
+  [ -z "$HASH" ] && HASH=DEV
+  VERSION="${BUILD_ID:-NONE}-$(echo ${HEALTH_APIS_VERSION}|tr . -)-${HASH}"
+  IMAGE="vasdvp/health-apis-upgraderator:$VERSION"
+  echo "Building upgraderator $VERSION"
+  
+  cat <<EOF > build.conf
 export VERSION="$VERSION"
 export BUILD_DATE="$(date)"
 export BUILD_HASH=$HASH
@@ -20,13 +72,14 @@ export BUILD_ID=${BUILD_ID:-NONE}
 export BUILD_BRANCH_NAME=${BRANCH_NAME:-NONE}
 export BUILD_URL="${BUILD_URL:-NONE}"
 EOF
+}
 
-set -e
-
-echo ------------------------------------------------------------
-docker build -t "$IMAGE" .
-docker images | grep health-apis-upgraderator
-
+buildUpgraderator() {
+  echo ------------------------------------------------------------
+  echo "Building upgraderator"
+  docker build -t "$IMAGE" .
+  docker images | grep health-apis-upgraderator
+}
 
 dockerRun() {
   docker run \
@@ -70,13 +123,13 @@ dockerRun() {
     $@
 }
 
-echo ------------------------------------------------------------
-
 blueGreen() {
  dockerRun --entrypoint /upgraderator/blue-green.sh $IMAGE $@
 }
 
 deleteOldVersions() {
+  echo ------------------------------------------------------------
+  echo Deleting old versions
   #
   # Delete all but the last few versions deployed (except if they are either blue or green)
   #
@@ -103,16 +156,11 @@ deleteVersion() {
   echo "Deleted $version"
 }
 
-[ -n "$SKIP_RUN" ] && exit 0
-
-#
-# Upgrade
-#
+set -e
+updateToLatestHealthApis
+configureUpgraderator
+buildUpgraderator
 dockerRun $IMAGE
-
-#
-# Clean up
-#
 deleteOldVersions
 
 
