c9a48e31407fa05cdc2a847ee8a7f4da966178ba
Bryan Schofield <bryan.schofield@libertyits.com>
2019-02-13 09:54:56 -0500

    Updated make-images to work in parallel and not steal window focus. Added support images to explain how kong could integrate into a string

--- images/make-images.sh
+++ images/make-images.sh
@@ -3,5 +3,6 @@
 which plantuml > /dev/null 2>&1
 [ $? != 0 ] && echo "plantuml not found" && exit 1
 
+export _JAVA_OPTIONS=-Djava.awt.headless=true
 cd $(dirname $(readlink -f $0))
-ls *.uml | xargs -n 1 plantuml
+ls *.uml | xargs -I {} -P 4 bash -c "echo Making {} ;  plantuml {} 2>/dev/null"
