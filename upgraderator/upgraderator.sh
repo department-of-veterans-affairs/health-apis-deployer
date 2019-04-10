ddb1ab8034492fe309bce872e47d49918829f047
Bryan Schofield <bryan.schofield@libertyits.com>
2019-04-10 15:36:48 -0400

    WIP Wed Apr 10 15:36:48 EDT 2019

--- upgraderator/upgraderator.sh
+++ upgraderator/upgraderator.sh
@@ -7,4 +7,9 @@ mkdir -p $WORK
 
 env | sort
 
+echo ------------------------------------------------------------
+aws ec2 describe-instances --filters Name=tag-key,Values=KubernetesCluster \
+  | jq -r '.Reservations[].Instances[]|.InstanceId,(.Tags[]|select(.Key=="KubernetesCluster").Value)' \
+  | paste -sd ' \n'
+
 exit 0
