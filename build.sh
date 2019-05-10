fe141d5a9594f9e2bb647541c6358a88a6aa4120
Ian LaFlamme <ian.laflamme@libertyits.com>
2019-05-10 13:00:28 -0400

    yaml validation

--- build.sh
+++ build.sh
@@ -55,11 +55,11 @@ EOF
 fetch-deployment-unit $DU_ARTIFACT $DU_VERSION
 tar xvf deployment-unit.tar.gz
 DU_DIR=$WORKSPACE/$DU_ARTIFACT-$DU_VERSION
-# TODO decrypt/expand *zip files in DU_DIR here
 unzip -d $DU_DIR -P $CRYPTO_KEY "$DU_DIR/*.zip"
 validate-deployment-unit $DU_DIR
 perform-substitution $DU_DIR
 # TODO sanitity check deployment.yaml here
+validate-yaml $DU_DIR/deployment.yaml
 cluster-fox copy-kubectl-config
 # TODO create namespace and ingress here
 cluster-fox kubectl us-gov-west-1a -- apply -f $DU_DIR/deployment.yaml
