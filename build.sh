908f8fdb2e65bb2605b14e9474bd92fd07972979
Bryan Schofield <bryan.schofield@libertyits.com>
2019-05-13 16:39:20 -0400

    k8s deployment test Mon May 13 16:39:20 EDT 2019

--- build.sh
+++ build.sh
@@ -74,8 +74,10 @@ perform-substitution $DU_DIR
 validate-yaml $DU_DIR/deployment.yaml $DU_NAMESPACE
 cluster-fox copy-kubectl-config
 apply-namespace-and-ingress $DU_DIR
+set +e
 cluster-fox kubectl $AVAILABILITY_ZONE -- get ns $DU_NAMESPACE -o yaml
 cluster-fox kubectl $AVAILABILITY_ZONE -- apply -v 5 -f $DU_DIR/deployment.yaml
+echo "KASJDKAJSDKJASKDJAKSJDAKJSDKAJSDKAJSDKAJSKDJAKSDJAKJDKAJSDKAJSDKAJDKJAKSDJAKSDJ"
 attach-deployment-unit-to-lb $CLUSTER_ID green $DU_HEALTH_CHECK_PATH \
   $DU_LOAD_BALANCER_RULE_PATH $DU_MIN_PRIORITY
 regression-test
