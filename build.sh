3f2617d3335ce84be020421ef22757a53a82c39a
jhulbert-va <43861345+jhulbert-va@users.noreply.github.com>
2019-05-29 08:38:38 -0400

    Filename Formatting

--- build.sh
+++ build.sh
@@ -132,7 +132,7 @@ cluster-fox copy-kubectl-config
 record-currently-installed-version ${AVAILABILITY_ZONES%% *} $PRIOR_CONF
 . $PRIOR_CONF
 
-export DEPLOYMENT_INFO_TEXT=DEPLOYMENT_INFO.txt
+export DEPLOYMENT_INFO_TEXT=deployment-info.txt
 
 cat <<EOF >> $DEPLOYMENT_INFO_TEXT
 ============================================================
