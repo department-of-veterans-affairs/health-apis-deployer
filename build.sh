a373a73373fda696a6ad6a1103ececd9bba1c9c6
jhulbert-va <joshua.hulbert@libertyits.com>
2019-06-25 10:38:38 -0400

    declare weak validation

--- build.sh
+++ build.sh
@@ -71,6 +71,7 @@ declare -x DU_DECRYPTION_KEY
 declare -x DU_HEALTH_CHECK_PATH
 declare -xA DU_LOAD_BALANCER_RULES # Associative array of priority to path
 declare -x DU_PROPERTY_LEVEL_ENCRYPTION
+declare -x WEAK_STRUCTURE_VALIDATION
 
 . $WORKSPACE/products/$PRODUCT.conf
 test -n "$DU_ARTIFACT"
