8955eb956762b748e03e517b522e849d51f0b5f6
Alec Bertossa <alec.bertossa@libertyits.com>
2020-01-09 18:12:26 -0600

    Slightly closer

--- bin/move-lb-rules.sh
+++ bin/move-lb-rules.sh
@@ -1,3 +1,5 @@
+
+
 usage() {
 cat<<EOF
 $0 [options]
@@ -11,6 +13,7 @@ Options:
  -d, --directory <dir>         What directory to look in
  -m, --decrement               Decrement (m)inus the delta
  -h, --help                    Display this help and exit
+
 $1
 EOF
   exit 1
@@ -39,19 +42,37 @@ done
 MIN=$1
 MAX=$2
 DELTA=$3
-PRIORITY_REGEX=".*\[(\d{2,3})\].*"
+PRIORITY_REGEX=".*\[([0-9]{2,3})"
 
 ((DELTA = $DELTA * $MODIFIER))
 
+if (( $MIN >= $MAX )); then
+  usage "Why did you do this? Max: $MAX, Min: $MIN"
+fi
+
 echo $DIRECTORY
 
-#Force these results to be an array so we can go line by line
-LB_RULES=($(grep -nro --include="*.conf" "DU_LOAD_BALANCER_RULES" $DIRECTORY))
+#Force these results to be an array so we can go line by line. Also cuts off after the
+#priority because we don't need it
+LB_RULES=($(grep -nr --include="*.conf" "DU_LOAD_BALANCER_RULES" $DIRECTORY | sed 's/].*//'))
 
+#Example line (start abbreviated): ../products/qms.conf:9:DU_LOAD_BALANCER_RULES[860
 for LINE in "${LB_RULES[@]}"; do
+  #Splits string into an array on : as a delimiter. Works by replacing the colon with a space
+  #to cause the string to be chunked like this, as I understand it
+  #https://www.linuxquestions.org/questions/programming-9/bash-shell-script-split-array-383848/#post3270796
   PARTS=(${LINE//:/ })
   FILE=${PARTS[0]}
   LINE_NUM=${PARTS[1]}
-  [[ "${PARTS[2]}" =~ $PRIORITY_REGEX ]]
-  echo ${BASH_REMATCH[1]}
+
+  #This feels like a gross way to do this, might have been better to just filter this through some stuff
+  [[ "${PARTS[2]}" =~ .*\[([0-9]{2,3}) ]]
+  PRIORITY=${BASH_REMATCH[1]}
+
+  #Are we in the range
+  if (( $PRIORITY >= $MIN )) && (( $PRIORITY <= $MAX )); then
+    ((NEW_PRIORITY= $PRIORITY + $DELTA))
+    echo $NEW_PRIORITY
+  fi
+
 done
