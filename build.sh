deee56f8431b579d85f007bb8a1cc47d0a0c280b
Bryan Schofield <bryan.schofield@libertyits.com>
2020-07-20 11:13:19 -0400

    development test Mon Jul 20 11:13:19 EDT 2020

--- build.sh
+++ build.sh
@@ -3,12 +3,33 @@ set +x -euo pipefail
 export PATH=${WORKSPACE:-.}/bin:$PATH
 
 cat <<EOF
+
+
 --- TODO ---
 - Default to 'latest' deploy-tools image
+
+
 EOF
 
+#============================================================
+onExit() {
+  STATUS=$?
+  if [ $STATUS -ne 0 ]
+  then
+    STAGE="$(stage current)"
+    deployment add-build-info \
+      -d "Stage \"$STAGE\" failed with status $STATUS"
+    stage start -s "CRASH AND BURN"
+    echo "OH NOES! SOMETHING BORKED!"
+    echo "TERMINATING WITH STATUS: $STATUS"
+  fi
+  exit $STATUS
+}
+trap onExit EXIT
+
+#============================================================
 initialize() {
-  banner h1 -m "Initializing"
+  stage start -s "Initializing"
   export NEXUS_URL=https://tools.health.dev-developer.va.gov/nexus/repository/health-apis-releases
   if [ -z "${VPC:-}" ]; then VPC=Dev; fi
   export ENVIRONMENT=$(vpc hyphenize -e "${VPC}")
@@ -28,7 +49,6 @@ setDeploymentId() {
   export DEPLOYMENT_ID="$prefix$ENVIRONMENT-$PRODUCT-$BUILD_NUMBER"
   export SHORT_DEPLOYMENT_ID="$short${ENVIRONMENT:0:1}${PRODUCT}${BUILD_NUMBER}"
 }
-
 
 #============================================================
 initDebugMode() {
